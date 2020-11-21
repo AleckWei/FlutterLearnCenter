300d481dbf425d1cf5be2733b8263bf6d1e3c31e
wwj <614505891@qq.com>
2020-11-21 23:19:49 +0800

    【flutter】【GridView 网格布局开了个头】

---
+++ FlutterLearning/flutter_app/lib/14main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'res/listData.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+// 使用ListView Builder并结合listData中的数组
+// 创建一个ListView
+
+//自定义组件 == 创建一个类：
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // MaterialApp 一般作为app的根组件
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: HomeContent(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class HomeContent extends StatelessWidget {
+  // List list = new List();
+  //
+  // // 在构造函数中添加一个List，后面用于在Container中展示
+  // HomeContent() {
+  //   for (var i = 1; i <= 20; i++) {
+  //     this.list.add(ListTile(
+  //           title: Text('我是第$i个标题'),
+  //           subtitle: Text('我是第$i条数据的副标题'),
+  //         ));
+  //     // this.list.add('我是第$i条数据');
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: listData.length,
+        itemBuilder: this._getListData
+        // 不带()表示函数的赋值，带了()表示函数的执行
+        );
+  }
+
+  Widget _getListData(context, index) {
+    return ListTile(
+      leading: Image.network('imageUrl'),
+      title: Text(listData[index]['title']),
+      subtitle: Text(listData[index]['author']),
+    );
+  }
+}
--- FlutterLearning/flutter_app/lib/main.dart
+++ FlutterLearning/flutter_app/lib/main.dart
@@ -4,8 +4,8 @@ import 'res/listData.dart';
 void main() {
   runApp(MyApp());
 }
-// 使用ListView Builder并结合listData中的数组
-// 创建一个ListView
+
+// GridView的最基本的使用
 
 //自定义组件 == 创建一个类：
 class MyApp extends StatelessWidget {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter Demo'),
         ),
-        body: HomeContent(),
+        body: LayoutDemo(),
       ),
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
@@ -26,35 +26,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// ignore: must_be_immutable
-class HomeContent extends StatelessWidget {
-  // List list = new List();
-  //
-  // // 在构造函数中添加一个List，后面用于在Container中展示
-  // HomeContent() {
-  //   for (var i = 1; i <= 20; i++) {
-  //     this.list.add(ListTile(
-  //           title: Text('我是第$i个标题'),
-  //           subtitle: Text('我是第$i条数据的副标题'),
-  //         ));
-  //     // this.list.add('我是第$i条数据');
-  //   }
-  // }
-
+class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: listData.length,
-        itemBuilder: this._getListData
-        // 不带()表示函数的赋值，带了()表示函数的执行
-        );
-  }
-
-  Widget _getListData(context, index) {
-    return ListTile(
-      leading: Image.network('imageUrl'),
-      title: Text(listData[index]['title']),
-      subtitle: Text(listData[index]['author']),
+    return GridView.count(
+      crossAxisCount: 2,
+      children: <Widget>[
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+        Text('这是一个文本'),
+      ],
     );
   }
 }
