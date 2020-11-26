d0a1f2431e119cd03d869b56b823326f589145dc
wwj <560391@gree.com.cn>
2020-11-26 19:25:55 +0800

    【flutter】【AppBar_Top 的 初始化 还未添加新代码】

---
+++ FlutterLearning/flutter_app/lib/forNow18_AppBar_Top/pages/user/RegisterFirst.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class RegisterFirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('第一步-输入密码')),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            Text('第一步-输入手机号'),
+            SizedBox(height: 40),
+            RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/registerSecond');
+                  // 路由栈的思想，向栈当中添加一个路由，然后栈中的路由对象就多一个
+                  // Navigator.of(context).pushReplacementNamed('/registerSecond');
+                  // 用replace的思想，用第二步的页面把第一步的替换掉
+                  // 但是这样会有一个问题，如果在第二步的时候，想修改第一步的信息，
+                  // 这样跳路由的话，一返回就重新回到“设置”页面，这样其实不友好
+                },
+                child: Text('下一步')),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ FlutterLearning/flutter_app/lib/forNow18_AppBar_Top/pages/user/RegisterThird.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/forNow18_AppBar_Top/pages/Tabs.dart';
+
+class RegisterThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('第三步-完成注册')),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            Text('输入密码完成注册：'),
+            SizedBox(height: 40),
+            RaisedButton(
+                onPressed: () {
+                  // Navigator.of(context).pop();
+                  // 如果前面的路由跳转使用的是push而不是replace的时候
+                  // 这个pop就会返回到第二步了
+
+                  // 下面就是直接返回根的方法：
+                  Navigator.of(context).pushAndRemoveUntil(
+                      new MaterialPageRoute(builder: (context) => new Tabs(index:2)),
+                      // 如果是直接调用Tabs()的话，默认是返回下标为0的页面
+                      (route) => route == null);
+                },
+                child: Text('确定')),
+          ],
+        ),
+      ),
+    );
+  }
+}
