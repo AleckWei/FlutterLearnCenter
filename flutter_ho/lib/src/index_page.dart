3e27780c195cdd2708bc8b3a599f208e474db025
wwj <614505891@qq.com>
2020-12-09 23:54:20 +0800

    【flutter】【《ho》将权限封装成一个widget，可以随时调用】【发现一个无法调到设置页面的问题】

--- flutter_ho/lib/src/index_page.dart
+++ flutter_ho/lib/src/index_page.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'package:permission_handler/permission_handler.dart';
+
+import 'pages/common/permission_request_widget.dart';
 
 class IndexPage extends StatefulWidget {
   @override
   _IndexPageState createState() => _IndexPageState();
 }
 
-class _IndexPageState extends State<IndexPage> with WidgetsBindingObserver {
+class _IndexPageState extends State<IndexPage> {
   // 保存弹出框文案的list
   List<String> _list = [
     '为您更好的体验应用，所以需要获取您手机文件存储权限，以保存您一些偏好设置',
@@ -17,36 +19,29 @@ class _IndexPageState extends State<IndexPage> with WidgetsBindingObserver {
     '其他错误'
   ];
 
-  // 是否去设置中心
-  bool _isGoSetting = false;
-
-  //页面初始化
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    checkPermission();
-    //  注册观察者
-    WidgetsBinding.instance.addObserver(this);
-  }
 
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    // TODO: implement didChangeAppLifecycleState
-    super.didChangeAppLifecycleState(state);
-    if(state == AppLifecycleState.resumed && _isGoSetting){
-      // 当app被重新启动
-      // 并且前往设置中心的标志位被设置为true时，就要再检查一次权限的状态
-      checkPermission();
-    }
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    // 注销观察者
-    WidgetsBinding.instance.removeObserver(this);
+    Future.delayed(Duration.zero, () {
+      Navigator.of(context)
+          .push(PageRouteBuilder(
+              opaque: false,
+              pageBuilder: (BuildContext context, Animation<double> animation,
+                  Animation<double> secondaryAnimation) {
+                return PermissionRequestWidget(
+                  permissionList: this._list,
+                  permission: Permission.camera,
+                );
+              }))
+          .then((value) {
+        if (value == null || value == false) {
+          //  权限请求不通过
+        } else {
+          //  权限请求通过
+        }
+      });
+    });
   }
 
   @override
@@ -61,82 +56,4 @@ class _IndexPageState extends State<IndexPage> with WidgetsBindingObserver {
       ),
     );
   }
-
-  void checkPermission({PermissionStatus status}) async {
-    //  动态获取权限
-    Permission permission = Permission.storage;
-
-    if (status == null) {
-      //  权限状态
-      status = await permission.status;
-    }
-
-    if (status.isUndetermined) {
-      //  如果用户之前从未申请过权限
-      showPermissionAlert(_list[0], "同意", permission);
-    } else if (status.isDenied) {
-      //  如果用户拒绝了这个权限
-      showPermissionAlert(_list[1], "重试", permission);
-    } else if (status.isPermanentlyDenied) {
-      //  如果用户拒绝了这个权限并且不再询问这个权限
-      showPermissionAlert(_list[2], "去设置中心", permission);
-    } else {
-      //  通过了权限申请
-    }
-  }
-
-  // 设置一个弹窗（苹果风格）
-  void showPermissionAlert(
-      String msg, String rightString, Permission permission,
-      {bool isSetting = false}) {
-    showCupertinoDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return CupertinoAlertDialog(
-            title: Text('温馨提示'),
-            content: Container(
-              padding: EdgeInsets.all(12),
-              child: Text(msg),
-            ),
-            actions: [
-              CupertinoDialogAction(
-                child: Text('退出应用'),
-                onPressed: () {
-                  onCloseApp();
-                },
-              ), // 左边的按钮
-              CupertinoDialogAction(
-                  child: Text('$rightString'),
-                  onPressed: () {
-                    // 申请权限前先关闭弹框
-                    Navigator.of(context).pop();
-
-                    if (isSetting) {
-                      _isGoSetting = true;
-                      //  去设置中心
-                      onAppSettings();
-                    } else {
-                      // 申请权限
-                      requestPermission(permission);
-                    }
-                  }), // 右边的按钮
-            ],
-          );
-        });
-  }
-
-  void requestPermission(Permission permission) async {
-    // 申请权限的主方法
-    PermissionStatus status = await permission.request();
-
-    // 申请之后需要再校验一次
-    checkPermission(status: status);
-  }
-
-  void onCloseApp() {
-    // 调用系统方法去关闭应用
-    SystemChannels.platform.invokeMapMethod("SystemNavigator.pop");
-  }
-
-  void onAppSettings() async {}
 }
---
+++ flutter_ho/lib/src/pages/common/permission_request_widget.dart
@@ -0,0 +1,163 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+// 权限请求模板
+class PermissionRequestWidget extends StatefulWidget {
+  final Permission permission;
+  final List<String> permissionList;
+  final bool isCloseApp;
+  final String leftButtonText;
+
+  PermissionRequestWidget(
+      {@required this.permission,
+      @required this.permissionList,
+      this.isCloseApp = false,
+      this.leftButtonText = '退出应用'});
+
+  @override
+  _PermissionRequestWidgetState createState() =>
+      _PermissionRequestWidgetState();
+}
+
+class _PermissionRequestWidgetState extends State<PermissionRequestWidget>
+    with WidgetsBindingObserver {
+  // 是否去设置中心
+  bool _isGoSetting = false;
+
+  //页面初始化
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkPermission();
+    //  注册观察者
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    // 注销观察者
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // TODO: implement didChangeAppLifecycleState
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed && _isGoSetting) {
+      // 当app被重新启动
+      // 并且前往设置中心的标志位被设置为true时，就要再检查一次权限的状态
+      checkPermission();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      //  只作权限处理，页面不要显示东西
+    );
+  }
+
+  void checkPermission({PermissionStatus status}) async {
+    //  动态获取权限
+    Permission permission = widget.permission; // 用全局变量的形式去读取我们要的权限
+
+    if (status == null) {
+      //  权限状态
+      status = await permission.status;
+    }
+
+    if (status.isUndetermined) {
+      //  如果用户之前从未申请过权限
+      showPermissionAlert(widget.permissionList[0], "同意", permission);
+    } else if (status.isDenied) {
+      if (Platform.isIOS) {
+        // ios平台没有拒绝后第二次请求，所以就得直接去设置中心了
+        showPermissionAlert(widget.permissionList[2], "去设置中心", permission);
+        return;
+      }
+      //  如果用户拒绝了这个权限
+      showPermissionAlert(widget.permissionList[1], "重试", permission);
+    } else if (status.isPermanentlyDenied) {
+      //  如果用户拒绝了这个权限并且不再询问这个权限
+      showPermissionAlert(widget.permissionList[2], "去设置中心", permission);
+    } else {
+      //  通过了权限申请
+      Navigator.of(context).pop(true);
+    }
+  }
+
+  // 设置一个弹窗（苹果风格）
+  void showPermissionAlert(
+      String msg, String rightString, Permission permission,
+      {bool isSetting = false}) {
+    showCupertinoDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoAlertDialog(
+            title: Text('温馨提示'),
+            content: Container(
+              padding: EdgeInsets.all(12),
+              child: Text(msg),
+            ),
+            actions: [
+              CupertinoDialogAction(
+                child: Text(
+                  '${widget.leftButtonText}',
+                  style: TextStyle(fontSize: 15),
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () {
+                  if (widget.isCloseApp) {
+                    onCloseApp();
+                  } else {
+                    Navigator.of(context).pop(false);
+                  }
+                },
+              ), // 左边的按钮
+              CupertinoDialogAction(
+                  child: Text(
+                    '$rightString',
+                    style: TextStyle(fontSize: 15),
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: () {
+                    // 申请权限前先关闭弹框
+                    Navigator.of(context).pop();
+                    if (isSetting) {
+                      _isGoSetting = true;
+                      //  去设置中心
+                      if (Platform.isAndroid) {
+                        print('安卓跳转');
+                      }
+                      openAppSettings(); // 调用系统方法前往设置中心
+                    } else {
+                      // 申请权限
+                      requestPermission(permission);
+                    }
+                  }), // 右边的按钮
+            ],
+          );
+        });
+  }
+
+  void requestPermission(Permission permission) async {
+    // 申请权限的主方法
+    PermissionStatus status = await permission.request();
+
+    // 申请之后需要再校验一次
+    checkPermission(status: status);
+  }
+
+  void onCloseApp() {
+    // 调用系统方法去关闭应用
+    SystemChannels.platform.invokeMapMethod("SystemNavigator.pop");
+  }
+}
