52051bc460264c0da8914c6b3c1d5943d7bbbbbd
wwj <560391@gree.com.cn>
2020-12-17 20:21:33 +0800

    【flutter】【《ho》富文本添加链接】

--- flutter_ho/lib/src/pages/common/protocol_model.dart
+++ flutter_ho/lib/src/pages/common/protocol_model.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ho/src/pages/common/common_webview.dart';
+import 'package:flutter_ho/src/utils/log_utils.dart';
+import 'package:flutter_ho/src/utils/navigator_utils.dart';
 
 class ProtocolModel {
-//  用来显示用户协议对话框
+  TapGestureRecognizer _registerProtocolRecognizer;
+  TapGestureRecognizer _privateProtocolRecognizer;
+
+  //  用来显示用户协议对话框
   Future<bool> showProtocolFunction(BuildContext context) async {
+    // 用户协议的手势
+    _registerProtocolRecognizer = TapGestureRecognizer();
+    // 隐私协议的手势
+    _privateProtocolRecognizer = TapGestureRecognizer();
+
     // 苹果风格的弹窗
     bool isShow = await showCupertinoDialog(
         context: context,
         builder: (BuildContext context) {
           return showProtocolDialog(context);
         });
-    return isShow;
+
+    // 弹窗结束后，销毁手势监听
+    _registerProtocolRecognizer.dispose();
+    _privateProtocolRecognizer.dispose();
+
+    return Future.value(isShow);
   }
 
   CupertinoAlertDialog showProtocolDialog(BuildContext context) {
     return CupertinoAlertDialog(
-      title: Text('温馨提示'),
+      title: Text('隐私协议'),
       content: Container(
+        height: 180,
         padding: EdgeInsets.all(12),
-        child: buildContent(context),
+        child: SingleChildScrollView(
+          child: buildContent(context),
+        ),
       ),
       actions: [
         // 这里面的数组是从左到右排列
@@ -54,16 +75,53 @@ class ProtocolModel {
       "如果您不能接受本协议中的全部条款，请勿开始使用本产品";
 
   buildContent(BuildContext context) {
+    // RichText富文本，可以设置文字高亮，连接等
     return RichText(
       text: TextSpan(
-        text: "请您在使用本产品之前仔细阅读",
         children: [
-          TextSpan(text: "《用户协议》"),
-          TextSpan(text: "与"),
-          TextSpan(text: "《隐私协议》"),
-          TextSpan(text: userPrivateProtocol),
+          TextSpan(
+              text: "      请您在使用本产品之前仔细阅读",
+              style: TextStyle(color: Colors.grey[600])),
+          TextSpan(
+            text: "《用户协议》",
+            style: TextStyle(color: Colors.blue),
+            recognizer: _registerProtocolRecognizer
+              ..onTap = () {
+                //  打开用户协议
+                openUserProtocol(context);
+              },
+          ),
+          TextSpan(text: "与", style: TextStyle(color: Colors.grey[600])),
+          TextSpan(
+            text: "《隐私协议》",
+            style: TextStyle(color: Colors.blue),
+            recognizer: _privateProtocolRecognizer
+              ..onTap = () {
+                //  打开隐私协议
+                openPrivateProtocol(context);
+              },
+          ),
+          TextSpan(
+              text: userPrivateProtocol,
+              style: TextStyle(color: Colors.grey[600])),
         ],
       ),
     );
+  }
+
+  // 查看用户协议
+  void openUserProtocol(BuildContext context) {
+    LogUtils.e("查看用户协议");
+    NavigatorUtils.pushPage(
+        context: context,
+        targetPage: CommonWebViewPage(htmlUrl: "https://www.baidu.com"));
+  }
+
+  // 查看隐私协议
+  void openPrivateProtocol(BuildContext context) {
+    LogUtils.e("查看隐私协议");
+    NavigatorUtils.pushPage(
+        context: context,
+        targetPage: CommonWebViewPage(htmlUrl: "https://www.baidu.com"));
   }
 }
